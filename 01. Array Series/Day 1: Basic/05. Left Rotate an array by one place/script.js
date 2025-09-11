1cdd18e98c514a86f8c413e46e00ef7fd3a2c007
Abhishek <abhishek84411@gmail.com>
2025-09-11 22:50:28 +0530

    Refactor left rotation logic and add maximum consecutive ones implementation with examples

--- 01. Array Series/Day 1: Basic/05. Left Rotate an array by one place/script.js
+++ 01. Array Series/Day 1: Basic/05. Left Rotate an array by one place/script.js
@@ -27,10 +27,11 @@ Space Complexity: O(1)
 // Left Rotate an array by D place
 
 const arr2 = [1, 2, 3, 4, 5, 6, 7];
-const d = 2;
+let d = 2;
 // Output: [3,4,5,6,7,1,2]
 
 // Brute Force Approach
+d = d % arr2.length; // if d is greater than array length
 let rotate = arr2.slice(0, d);
 for (let i = d; i < arr2.length; i++) {
   arr2[i - d] = arr2[i];
