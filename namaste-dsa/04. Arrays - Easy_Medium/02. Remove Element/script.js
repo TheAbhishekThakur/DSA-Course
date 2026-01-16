3bfb961672abdd092f1183d09a59b5b248a9f303
Abhishek Thakur <55867934+TheAbhishekThakur@users.noreply.github.com>
2026-01-16 17:05:21 +0000

    comment added for remove array value

--- namaste-dsa/04. Arrays - Easy_Medium/02. Remove Element/script.js
+++ namaste-dsa/04. Arrays - Easy_Medium/02. Remove Element/script.js
@@ -43,3 +43,8 @@ console.log(result1); // Output: 2, nums = [2,2,_,_]
 
 const result2 = removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
 console.log(result2); // Output: 5, nums = [0,1,4,0,3,_,_,_]
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
